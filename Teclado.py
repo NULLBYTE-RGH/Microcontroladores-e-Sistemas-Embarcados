35c4c37067920d9427e6a04309e5890f1c0c321b
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2022-05-27 00:51:44 -0300

    Update Teclado.py

--- Teclado.py
+++ Teclado.py
@@ -45,13 +45,46 @@ class Teclado(object):
 
         return tecla
 
-    def LerTeclado(self):
-        while True:
+    def LerTeclado(self, Numero_de_caracteres):
+        lidos = ''
+        while len(lidos) < Numero_de_caracteres:
             for Linha in range(4):
                 for Coluna in range(4):
                     tecla = self.__Ler_Teclado(Linha,Coluna)
                     if  tecla == 1:
-                        print("Tecla :", __teclas[Linha][Coluna])
+                        #print("Tecla :", __teclas[Linha][Coluna])
+                        tecla = __teclas[Linha][Coluna]
+                        if tecla != '':
+                            lidos = lidos + tecla
                         sleep(0.5)
+        return lidos
+
+    def Criar_Senha(self, id, Tamanho_Senha):
+        
+        try:
+            arquivo = open('Usuarios.txt', 'r')
+            usuarios = []
+            for linha in arquivo:
+                item = linha.split(",")
+                item[4] = item[4].replace("\r\n","")
+                item1 = item[0:2]
+                item2 = item[3:]
+                
+                if str(item1[0]) == str(id):
+                    print("Cadastrando Senha para "+ item1[1])
+                    senha = self.LerTeclado(Tamanho_Senha)
+                    item1.append(senha)
+                    usuarios.append(item1+item2)
+                else:
+                    usuarios.append(item1+[item[2]]+item2)
                     
+            arquivo.close()
+            arquivo = open('Usuarios.txt', 'w')
+            for i in usuarios:
+                arquivo.write(",".join(i)+"\r\n")
+            arquivo.close()
+            return 1
+        except Exception as e:
+            print(e)
+            return -1
 
